7c73c5076b997e2ee6628f693fbef2c053e74e07
notZuri <blankbringer@gmail.com>
2025-07-06 23:06:48 +0800

    login_sample

--- lockedin2.js
+++ lockedin2.js
@@ -1,40 +1,10 @@
+let userName = "Ian";
+let password = "password123";
 
-function isLoggedIn(status){
-    console.log(`Si user ay  ${status}`);
+if (userName === "Ian" && password === "password123") {
+    alert("Login successful!");
+} else if (userName === "Ian" && password !== "password123") {
+    alert("Incorrect password");
+} else{
+    alert("User not found");
 }
-
-isLoggedIn(`naka login`);
-
-
-    
-    let isAdmin = true;
-
-if (isAdmin) {
-  alert("Welcome, Admin!");
-} else {
-  alert("Access denied.");
-}
-
-
-function logIn(username, password) {
-  if (username === "Ian" && password === "12345") 
-    console.log("Login successful");
-   else {
-    console.log("Login failed");
-  }
-}
-
-logIn("Ian", "12345"); // ✅ Login successful
-
-
-
-
-function login(inputUser, inputPass){
-    if (inputUser === `Ian` && inputPass === `abc123`) {
-        console.log(`Welcome, Ian!`)
-    } else {
-        console.log(`Invalid credentials.`);
-    }
-}
-
-    login(`Ian`, `abc123`);

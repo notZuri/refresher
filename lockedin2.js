0f892c77c518207712f01700a15dc2bbf1a22b8c
notZuri <blankbringer@gmail.com>
2025-07-06 22:16:46 +0800

    challenge

---
+++ lockedin2.js
@@ -0,0 +1,15 @@
+let genAve = 69;
+
+if (genAve >= 101){
+    console.log(`Invalid Grade`);
+} else if (genAve >= 98){
+    console.log(`With Highest Honors`);
+} else if (genAve >= 95){
+    console.log(`With High Honors`);
+} else if (genAve >= 90){  
+    console.log(`With Honors`);
+} else if (genAve >= 75){
+    console.log(`Passed`);
+} else{
+    console.log(`Failed`);
+}

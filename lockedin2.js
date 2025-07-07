8b05924014676a137bb71cc8e4ae70cf93ab9112
notZuri <blankbringer@gmail.com>
2025-07-07 11:52:01 +0800

    !not_condition

--- lockedin2.js
+++ lockedin2.js
@@ -5,6 +5,4 @@ if (userName === "Ian" && password === "password123") {
     alert("Login successful!");
 } else if (userName === "Ian" && password !== "password123") {
     alert("Incorrect password");
-} else{
-    alert("User not found");
 }

1ddaf68f562907f57c64c374d26bcc0de217c9f3
notZuri <blankbringer@gmail.com>
2025-07-10 23:49:26 +0800

    challenge

--- lockedin2.js
+++ lockedin2.js
@@ -1,7 +1,7 @@
-let years = 4;
+/*let years = 4;
 let language = `js`;
 
-//alert(!((years > 5) && (language === `asd`)));
+alert(!((years > 5) && (language === `asd`)));
 
 
 let isGood = false;
@@ -11,4 +11,14 @@ if (!(isGood && malakas)) {
     console.log(`Galing mo boss!`);
 } else {
     console.log(`Hindi ka magaling boss!`);
+}*/
+
+
+let name = `John`;
+let course = `JavaScript`;
+
+if (!(name === `John` && course === `JavaScript`)) {
+    console.log(`You are not John and you are not taking JavaScript.`);
+}  else {
+    console.log(`You are John and you are taking JavaScript.`);
 }
